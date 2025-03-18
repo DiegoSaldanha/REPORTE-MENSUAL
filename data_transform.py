c256ad26f879b799f915ebec5e533d9bb23df53c
Diego <diegosaldana2701@gmail.com>
2025-03-18 14:36:00 -0500

    Primera version del archivo data_transform

---
+++ data_transform.py
@@ -0,0 +1,18 @@
+#regla 1
+
+import pandas as pd
+#ruta del archivo csv intermedio
+ try: 
+#leer el archivo csv
+data = pd.read_csv(archivo_csv)
+
+#ordenar los datos por nombre
+data_ordenada = data.sort_values(by='Nombre1')
+
+# Exportar a Excel
+archivo_excel = 'clientes_ordenados.xlsx'
+data_ordenada.to_excel(archivo_excel, index=False)
+    
+print(f"Datos exportados exitosamente a {archivo_excel}")
+except Exception as e:
+print(f"Error al transformar los datos: {e}")
